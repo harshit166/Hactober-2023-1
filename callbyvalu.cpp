60fcc78af7dea78a2d6866e180d30ca3e49a6c97
harshit166 <147302987+harshit166@users.noreply.github.com>
2023-10-28 20:47:07 +0530

    Create callbyvalu.cpp

---
+++ callbyvalu.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Swap function to demonstrate
+// call by value method
+void swap(int x, int y)
+{
+	int t = x;
+	x = y;
+	y = t;
+	cout << "After Swapping in function x: " << x
+		<< ", y: " << y << endl;
+}
+int main()
+{
+	int x = 1, y = 2;
+
+	cout << "Before Swapping: ";
+	cout << "x: " << x << ", y: " << y << endl;
+
+	swap(x, y);
+
+	cout << "After Swapping: ";
+	cout << "x: " << x << ", y: " << y << endl;
+
+	return 0;
+}
